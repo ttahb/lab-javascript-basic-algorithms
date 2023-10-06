13bae39efa754d3567618416cd944b0232dae58c
ttahb <bhattvijay69@hotmail.com>
2023-10-06 18:59:41 +0530

    Lab1 firest commit

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // Iteration 1: Names and Input
 
+let driverName = "Ned Stark";
+console.log(`The driver's name is ${driverName}`);
+
+let navigatorName = "Jon Snow";
+console.log(`The navigator's name is ${navigatorName}`);
 
 // Iteration 2: Conditionals
-
+if(driverName.length > navigatorName.length){
+    console.log(`The driver has the longest name, it has ${driverName.length} characters.`)
+} else if( driverName.length < navigatorName.length){
+    console.log(`It seems that the navigator has the longest name, it has ${navigatorName.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${driverName.length} characters!`);
+}
 
 // Iteration 3: Loops
+
+//3.1
+let driverName2 = "";
+for(let i=0; i<driverName.length - 1; i++){
+    driverName2 += driverName[i].toUpperCase() + ' ';
+}
+driverName2 += driverName[driverName.length -1].toUpperCase();
+
+console.log(`The driver's name in capitals and with space between letters is ${driverName2}`);
+
+//3.2
+let reversedNavigatorName = "";
+for(let i=navigatorName.length-1; i>=0; i--){
+    reversedNavigatorName += navigatorName[i];
+}
+
+console.log(`Navigator's name in reverse is ${reversedNavigatorName}`);
+
+//3.3
+
+
